eff7a1b0701059f12cbb519532687b1779686cee
Greg Netols <44214450+GregNetolsOpex@users.noreply.github.com>
2018-10-16 17:46:51 -0500

    Update Week1.py
    Small start

---
+++ Week1.py
@@ -0,0 +1,23 @@
+import numpy
+
+
+class OrderQuantity(object):
+    """docstring for OrderQuantity."""
+    def __init__(self, Q):
+        self.Q = Q
+
+    def pull(self):
+        '''Performs a random "pull" and returns the reward'''
+        pass
+
+    def update(self):
+        '''Updates the estimate of the mean reward'''
+        pass
+
+
+def main():
+    pass
+        
+
+if __name__ == "main":
+    main()
