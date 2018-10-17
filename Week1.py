13663fd902f32ddc23c8e938c2325d48f514c040
Greg Netols <44214450+GregNetolsOpex@users.noreply.github.com>
2018-10-17 16:18:41 -0500

    Initial Commit

--- Week1.py
+++ Week1.py
@@ -1,13 +1,19 @@
-import numpy
-
+import numpy as np
 
 class OrderQuantity(object):
     """docstring for OrderQuantity."""
-    def __init__(self, Q):
+    def __init__(self, Q, Hc, Sc):
         self.Q = Q
+        self.Hc = Hc
+        self.Sc = Sc
 
     def pull(self):
-        '''Performs a random "pull" and returns the reward'''
+        '''
+        Performs a random "pull" and returns the reward
+        Returns: The cost incurred through one iteration of the newsvendor game
+        '''
+        self.Q = self.Q - np.random.normal(loc=20, scale=4)
+
         pass
 
     def update(self):
@@ -16,8 +22,11 @@ class OrderQuantity(object):
 
 
 def main():
+
+    newsvendor_instance = OrderQuantity(Q=15,Hc=1,Sc=20)
+
     pass
-        
+
 
 if __name__ == "main":
     main()
